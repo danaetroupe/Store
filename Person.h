b5726d4021e2eab15249d1d145f3b62632ecea7f
Danae Troupe <danaetroupe@gmail.com>
2023-09-29 10:40:18 -0400

    Completed all header files

--- Person.h
+++ Person.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <string>
+#include <map>
+#include "Account.h"
+using namespace std;
+
 class Person
 {
+public:
+	Person(int userId, string name);
+	Person(int userId, string name, float accountBalance);
+	~Person();
+
+	bool checkId(int id);
+	void addItem(string name, int quantity);
+private:
+	string name;
+	Account* account;
+	int userId;
+	map<string, int> inventory;
 };
 
--- Store.h
+++ Store.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <string>
+#include "Person.h"
+using namespace std;
+
+struct Item {
+	string name;
+	float price;
+	int quantity;
+};
+
 class Store
 {
+public:
+	Store();
+	
+	void addItem(string name, float price, int quantity);
+	bool buyItem(Person* person, string itemName);
+
+private:
+	map<string, Item> inventory;
 };
 
