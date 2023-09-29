42736f6734406b1ab8aab107336ee6c72252f04f
Danae Troupe <danaetroupe@gmail.com>
2023-09-29 10:48:17 -0400

    Created definitions using VS Code

--- Person.cpp
+++ Person.cpp
@@ -1,1 +1,23 @@
 #include "Person.h"
+Person::Person(int userId, string name)
+{
+    this->userId = userId;
+    this->name = name;
+}
+
+Person::Person(int userId, string name, float accountBalance)
+{
+}
+
+Person::~Person()
+{
+}
+
+bool Person::checkId(int id)
+{
+    return false;
+}
+
+void Person::addItem(string name, int quantity)
+{
+}
--- Store.cpp
+++ Store.cpp
@@ -1,1 +1,14 @@
 #include "Store.h"
+
+Store::Store()
+{
+}
+
+void Store::addItem(string name, float price, int quantity)
+{
+}
+
+bool Store::buyItem(Person *person, string itemName)
+{
+    return false;
+}
