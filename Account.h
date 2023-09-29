e76222361252ff8230778858bd5fce57462ebef7
Danae Troupe <danaetroupe@gmail.com>
2023-09-29 10:26:01 -0400

    Finished Account Header - all definitions inline

--- Account.h
+++ Account.h
@@ -2,11 +2,21 @@
 class Account
 {
 public:
+	Account() { accountBalance = 0; }
 	Account(float balance) { accountBalance = balance; }
 
+	void despoitMoney(float money) { accountBalance += money; }
+	float getCurrentBalance() { return accountBalance; }
 
+	// Return true if money can be spent, false if not
+	bool spendMoney(float money) {
+		if (money <= accountBalance) {
+			accountBalance -= money;
+			return true;
+		}
+		else { return false; }
+	}
 private: 
 	float accountBalance;
-
 };
 
